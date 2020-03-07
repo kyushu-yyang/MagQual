d0c2fca85d0b13c42045df04930e19be9afd56a4
kyushu-yyang <yangyefactor@gmail.com>
2020-03-07 21:24:34 +0900

    first commit

---
+++ include/TFieldHomogeneity.h
@@ -0,0 +1,16 @@
+#ifndef TFIELDHOMOGENEITY_HH
+#define TFIELDHOMOGENEITY_HH
+
+// the base class to calculate the field homogeneity
+// Author: Y.Yang
+// Date  : 2020.03.07
+
+
+class TFieldHomogeneity
+{
+
+
+};
+
+
+#endif
---
+++ src/TFieldHomogeneity.cxx
@@ -0,0 +1,4 @@
+
+#include "TFieldHomogeneity.h"
+
+
